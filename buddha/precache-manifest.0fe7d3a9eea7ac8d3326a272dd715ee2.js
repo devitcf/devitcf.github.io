636156b6350bf1e4c55e02d78560d4a20d9e2ae1
Devi Tse <>
2021-08-23 11:39:06 +0800

    Add PWA support, add demo survey dialog

---
+++ buddha/precache-manifest.0fe7d3a9eea7ac8d3326a272dd715ee2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b4e4d40a6ccca476c9",
+    "url": "css/app.d0f1b1ce.css"
+  },
+  {
+    "revision": "352c11f0b920116a2f8c",
+    "url": "css/chunk-vendors.ea3b5c37.css"
+  },
+  {
+    "revision": "f4303f8e3ab3c68f5cf1db08750ff7d2",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "8fd34a1dc7ec7e2937a376a6e4d5e722",
+    "url": "fonts/material-icons.8fd34a1d.woff2"
+  },
+  {
+    "revision": "ef85b3d7ffe53adfbdcab4c157013dcf",
+    "url": "fonts/material-icons.ef85b3d7.woff"
+  },
+  {
+    "revision": "f4303f8e3ab3c68f5cf1db08750ff7d2",
+    "url": "img/app-logo.f4303f8e.png"
+  },
+  {
+    "revision": "f1f324033af837d67112aaa67232008e",
+    "url": "img/buddha-icon.f1f32403.png"
+  },
+  {
+    "revision": "461a8e02f7fb019e9e7b137b168e5d2e",
+    "url": "img/jesus-icon.461a8e02.png"
+  },
+  {
+    "revision": "8395ad69a7273b4b17b7881aa0005412",
+    "url": "index.html"
+  },
+  {
+    "revision": "f0b4e4d40a6ccca476c9",
+    "url": "js/app.bf5efaee.js"
+  },
+  {
+    "revision": "352c11f0b920116a2f8c",
+    "url": "js/chunk-vendors.a01c86a3.js"
+  },
+  {
+    "revision": "e83f3b85aaded64826d97e02671ea73a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
